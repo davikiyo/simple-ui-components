9d82b8b594fefa241d55ff792334abef8c310178
davikiyo <davikiyo@gmail.com>
2022-11-23 14:12:26 +0900

    Update the storybook setting
    Switches to `react-docgen-typescript` for extended types.

--- stories/UI/IconButton.stories.tsx
+++ stories/UI/IconButton.stories.tsx
@@ -1,16 +1,9 @@
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
 
 import { IconButton } from 'components'
-import { ICON_NAMES } from 'types/Icon'
 
 export default {
   component: IconButton,
-  argTypes: {
-    icon: {
-      control: 'select',
-      options: ICON_NAMES,
-    },
-  },
 } as ComponentMeta<typeof IconButton>
 
 export const Normal: ComponentStoryObj<typeof IconButton> = {
