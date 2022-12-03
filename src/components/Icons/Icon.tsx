36beb6628adde655e54fdebbcc170060a502e00f
davikiyo <davikiyo@gmail.com>
2022-12-03 15:54:16 +0900

    Fixed the icon issue
    Fixed the issue where icons were not displayed in Safari.

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from 'react'
 
 import { styled, ROOT_FONT_SIZE } from 'styles'
-import svgSrc from 'assets/icons.svg'
+import { ReactComponent as SVGSprite } from 'assets/icons.svg'
 import { IconName } from 'types/Icon'
 
 export interface IconProps {
@@ -49,18 +49,13 @@ const Icon = forwardRef<HTMLSpanElement, IconProps>(function (
       className={className}
       css={{
         color,
-        maxWidth: `${width / ROOT_FONT_SIZE}rem`,
-        maxHeight: `${height / ROOT_FONT_SIZE}rem`,
+        width: `${width / ROOT_FONT_SIZE}rem`,
+        height: `${height / ROOT_FONT_SIZE}rem`,
       }}
     >
-      <svg
-        role="img"
-        aria-label={name}
-        width={`${width / ROOT_FONT_SIZE}rem`}
-        height={`${height / ROOT_FONT_SIZE}rem`}
-        fill="currentColor"
-      >
-        <use href={`${svgSrc}#${name}`} />
+      <SVGSprite />
+      <svg role="img" aria-label={name} height="100%" width="100%" fill="currentColor">
+        <use href={`#icon-${name}`} />
       </svg>
     </IconContainer>
   )
