c2711d329bda7b062405f841bfd1918caae9661c
davikiyo <davikiyo@gmail.com>
2023-05-09 21:07:45 +0900

    Upgraded the Storybook and migrated to SWC
    Still requires `ts-jest` as a dependency because
    `pathsToModuleNameMapper` is not supported in `@swc/jest` yet.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
 import svgr from 'vite-plugin-svgr'
 
